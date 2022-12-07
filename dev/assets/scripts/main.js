a7598b9a06a929ddc58f10b7285829f2ab5d836f
AndreyFront <andrey-se-v@yandex.ru>
2022-12-07 17:40:35 +0300

    desktop header, footer, performance is made

--- dev/assets/scripts/main.js
+++ dev/assets/scripts/main.js
@@ -1,3 +1,19 @@
 window.onload = () => {
-    
+    function fixedHeader() {
+        const header = document.querySelector('[data-header="main"]')
+
+        if (!header) return
+
+        window.addEventListener('scroll', (event) => {
+            const scrolled = window.pageYOffset ? window.pageYOffset : document.body.scrollTop;
+
+            if (scrolled >= 50) {
+                header.classList.add('header--fixed')
+            } else {
+                header.classList.remove('header--fixed')
+            }
+        })
+    }
+
+    fixedHeader()
 }
